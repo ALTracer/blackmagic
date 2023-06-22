d30a208a70e31fd12498d4bed547b605e44bd417
ALTracer <tolstov_den@mail.ru>
2023-06-22 08:28:37 +0300

    target/lpc546xx: add SRAMX to mapping
    * Declare the SRAMX on I&D buses at 0x0400_0000
    * Fix case of adjacent string literal

--- src/target/lpc546xx.c
+++ src/target/lpc546xx.c
@@ -161,7 +161,8 @@ bool lpc546xx_probe(target_s *t)
 	const uint32_t sram0_size = 64U * 1024U;
 	target_add_ram(t, 0x20000000, sram0_size);
 	target_add_ram(t, 0x20010000, sram123_size);
-	target_add_commands(t, lpc546xx_cmd_list, "Lpc546xx");
+	target_add_ram(t, 0x04000000, 0x8000U); /* SRAMX */
+	target_add_commands(t, lpc546xx_cmd_list, "LPC546xx");
 	t->target_options |= CORTEXM_TOPT_INHIBIT_NRST;
 	return true;
 }
