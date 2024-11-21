65c83475539fb2c151f4192718fcd47ae0dabcdb
ALTracer <altracer@noreply.codeberg.org>
2024-11-21 13:16:56 +0300

    cortexm: Allow toggling non-halting option via a runtime command

--- src/target/cortexm.c
+++ src/target/cortexm.c
@@ -44,6 +44,9 @@
 #include "semihosting.h"
 #include "platform.h"
 #include "maths_utils.h"
+#ifdef ENABLE_RTT
+#include "rtt.h" // extern bool rtt_found;
+#endif
 
 #include <assert.h>
 
@@ -51,9 +54,15 @@
 #define CORTEXM_MAX_REG_COUNT (CORTEXM_GENERAL_REG_COUNT + CORTEX_FLOAT_REG_COUNT + CORTEXM_TRUSTZONE_REG_COUNT)
 
 static bool cortexm_vector_catch(target_s *target, int argc, const char **argv);
+#ifdef ENABLE_RTT
+static bool cortexm_mem_nohalt(target_s *target, int argc, const char **argv);
+#endif
 
 const command_s cortexm_cmd_list[] = {
 	{"vector_catch", cortexm_vector_catch, "Catch exception vectors"},
+#ifdef ENABLE_RTT
+	{"mem_nohalt", cortexm_mem_nohalt, "Toggle halting during memory accesses"},
+#endif
 	{NULL, NULL, NULL},
 };
 
@@ -1326,6 +1335,30 @@ static bool cortexm_vector_catch(target_s *target, int argc, const char **argv)
 	tc_printf(target, "\n");
 	return true;
 }
+
+#ifdef ENABLE_RTT
+static bool cortexm_mem_nohalt(target_s *target, int argc, const char **argv)
+{
+	bool enable = false;
+	if (argc > 2) {
+		tc_printf(target, "Usage: monitor mem_nohalt <enable|disable>");
+		return false;
+	}
+	if (argc == 2 && parse_enable_or_disable(argv[1], &enable)) {
+		if (enable)
+			target->target_options |= TOPT_NON_HALTING_MEM_IO;
+		else
+			target->target_options &= ~TOPT_NON_HALTING_MEM_IO;
+		/* Force reapplying halt settings in rtt.c */
+		rtt_found = false;
+
+		return true;
+	}
+	tc_printf(target, "Target allows non-halting memory IO: %s\n",
+		target->target_options & TOPT_NON_HALTING_MEM_IO ? "yes" : "no");
+	return true;
+}
+#endif
 
 static bool cortexm_hostio_request(target_s *const target)
 {
