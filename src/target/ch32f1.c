bff6eed083b93fa0f7a4c033b4cc474a44b8949d
ALTracer <11005378+ALTracer@users.noreply.github.com>
2026-02-23 13:39:05 +0300

    ch32f1: Log whether resetting fast unlock fails in probe path

--- src/target/ch32f1.c
+++ src/target/ch32f1.c
@@ -144,12 +144,14 @@ static bool ch32f1_flash_lock(target_s *const target)
  */
 static bool ch32f1_has_fast_unlock(target_s *const target)
 {
-	DEBUG_INFO("CH32: has fast unlock\n");
+	DEBUG_INFO("CH32: has fast unlock?\n");
 	// reset fast unlock
 	ch32f1_flash_ctrl_set(target, CH32F1_FLASH_CTRL_FLOCK);
 	platform_delay(1); // The flash controller is timing sensitive
-	if (!(target_mem32_read32(target, CH32F1_FLASH_CTRL) & CH32F1_FLASH_CTRL_FLOCK))
+	if (!(target_mem32_read32(target, CH32F1_FLASH_CTRL) & CH32F1_FLASH_CTRL_FLOCK)) {
+		DEBUG_INFO("CH32: check for fast unlock failed\n");
 		return false;
+	}
 	// send unlock sequence
 	target_mem32_write32(target, CH32F1_FLASH_KEY, CH32F1_FLASH_KEY1);
 	target_mem32_write32(target, CH32F1_FLASH_KEY, CH32F1_FLASH_KEY2);
