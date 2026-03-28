154984066d4295b30391f75a271a43fb2788b899
ALTracer <11005378+ALTracer@users.noreply.github.com>
2026-03-28 11:13:18 +0300

    mdr32vf: Flash driver skeleton

--- src/target/mdr32vf.c
+++ src/target/mdr32vf.c
@@ -38,6 +38,34 @@
 
 #define MDR32F02FI_FLASH_BASE 0x10000000U
 
+static bool mdr32vf_flash_prepare(target_flash_s *flash);
+static bool mdr32vf_flash_erase(target_flash_s *flash, target_addr_t addr, size_t len);
+static bool mdr32vf_flash_write(target_flash_s *flash, target_addr_t dest, const void *src, size_t len);
+static bool mdr32vf_flash_done(target_flash_s *flash);
+static bool mdr32vf_mass_erase(target_flash_s *flash, platform_timeout_s *print_progress);
+
+static void mdr32vf_add_flash(
+	target_s *const target, const target_addr32_t addr, const size_t length, const size_t erasesize)
+{
+	target_flash_s *flash = calloc(1, sizeof(*flash));
+	if (!flash) { /* calloc failed: heap exhaustion */
+		DEBUG_ERROR("calloc: failed in %s\n", __func__);
+		return;
+	}
+
+	flash->start = addr;
+	flash->length = length;
+	flash->blocksize = erasesize;
+	flash->writesize = 1024U;
+	flash->erased = 0xff;
+	flash->prepare = mdr32vf_flash_prepare;
+	flash->erase = mdr32vf_flash_erase;
+	flash->mass_erase = mdr32vf_mass_erase;
+	flash->write = mdr32vf_flash_write;
+	flash->done = mdr32vf_flash_done;
+	target_add_flash(target, flash);
+}
+
 bool mdr32f02fi_probe(target_s *const target)
 {
 	const riscv_hart_s *hart = (riscv_hart_s *)target->priv;
@@ -48,7 +76,49 @@ bool mdr32f02fi_probe(target_s *const target)
 	target_add_ram32(target, MDR32F02FI_SRAM_AHB_BASE, 16U * 1024U);
 	target_add_ram32(target, MDR32F02FI_SRAM_TCMA_BASE, 64U * 1024U);
 	target_add_ram32(target, MDR32F02FI_SRAM_TCMB_BASE, 32U * 1024U);
-	/* Flash: 256 KiB, driver: TODO */
-	target_add_ram32(target, MDR32F02FI_FLASH_BASE, 256U * 1024U);
+	/* Flash: 256 KiB in 64 sectors of 4 KiB, driver: MDR187 */
+	mdr32vf_add_flash(target, MDR32F02FI_FLASH_BASE, 256U * 1024U, 4096U);
 	return true;
+}
+
+static bool mdr32vf_flash_unlock(target_s *const target)
+{
+	(void)target;
+	return false;
+}
+
+static bool mdr32vf_flash_prepare(target_flash_s *const target_flash)
+{
+	target_s *target = target_flash->t;
+	return mdr32vf_flash_unlock(target);
+}
+
+static bool mdr32vf_flash_done(target_flash_s *const target_flash)
+{
+	(void)target_flash;
+	return false;
+}
+
+static bool mdr32vf_flash_erase(target_flash_s *flash, target_addr_t addr, size_t len)
+{
+	(void)flash;
+	(void)addr;
+	(void)len;
+	return false;
+}
+
+static bool mdr32vf_flash_write(target_flash_s *flash, target_addr_t dest, const void *src, size_t len)
+{
+	(void)flash;
+	(void)dest;
+	(void)src;
+	(void)len;
+	return false;
+}
+
+static bool mdr32vf_mass_erase(target_flash_s *flash, platform_timeout_s *print_progress)
+{
+	(void)flash;
+	(void)print_progress;
+	return false;
 }
